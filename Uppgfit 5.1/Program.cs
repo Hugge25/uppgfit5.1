284a0ebf3282e98bd0e1862caab0277c0025415d
Hugge25 <hugo.paulsson@hotmail.com>
2023-01-20 10:10:07 +0100

    Add project files.

---
+++ Uppgfit 5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] v = {"Hej på dig", "Vad gör du?", "Jag är cool"};
+            
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(v[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
